597cb62fc500f8e201f8f9c116f4ee2ce1c3e85e
FernB <fernbradder@gmail.com>
2021-06-04 16:12:29 +0800

    final app

--- scrape_mars.py
+++ scrape_mars.py
@@ -16,42 +16,35 @@ def scrape():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
 
-    # Visit visitcostarica.herokuapp.com
-  
-
+    #visit news site
     browser.visit(news_url)
     html = browser.html
     soup = bs(html, 'lxml')
 
-
+    # find and save title and content of first article
     news_title = soup.find('div', class_="content_title").get_text()
     news_p = soup.find('div', class_='article_teaser_body').get_text()
     
 
-    # Scrape page into Soup
-
+    # visit image site
     browser.visit(feature_url)
+
+    # find and save link to feature image
     feature_image = browser.links.find_by_partial_href('feature').first['href']
-    # feature_image = feature_image[8:]
     
-
-
-
-
+    # visit geological site
     browser.visit(astro_geo_url)
+
+    # find all links to image sites
     image_links = browser.find_by_css('.product-item').links.find_by_partial_text('Enhance')
 
+    # create list of urls
     listoflinks = []
     for i in range(4):
         listoflinks.append(image_links[i]['href'])
 
-    # tifflist = {}
-    # for i in listoflinks:
-    #     browser.visit(i)
-    #     tiff = browser.links.find_by_text('Original')
-    #     name = browser.find_by_css('.title').first.value.rstrip('Enhanced').strip()
-    #     tifflist[name] = tiff['href']
 
+    # visit urls and find and save jpeg images
     imglist = []
     for i in listoflinks:
         browser.visit(i)
@@ -64,30 +57,13 @@ def scrape():
         imglist.append(dic)
 
 
-    # tifflist = []
-    # for i in listoflinks:
-    #     browser.visit(i)
-    #     tiff = browser.links.find_by_text('Sample')
-    #     name = browser.find_by_css('.title').first.value.rstrip('Enhanced').strip()
-    #     dic = {}
-    #     dic['title'] = name
-    #     dic['img_url'] = tiff['href']
-            
-    #     tifflist.append(dic)
-    
-
-
-
+    # pandas scrape comparison table
     tables = pd.read_html(facts_url)
     df = tables[0]
     df.rename(columns=df.iloc[0], inplace=True)
     df.drop([0], inplace=True)
-    html_table = df.to_html(index=False)
-
-
-
-
-
+    # save at html table
+    html_table = df.to_html(index=False, bold_rows=True, col_space=20, classes=["table-striped", "table-bordered", "tstyle"])
 
 
     # Store data in a dictionary
